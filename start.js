5a9266d3e80e44c5741fa792233a2f3cff1cc169
ruibz <rui.b.zhang@hotmail.com>
2017-08-10 11:08:13 +0800

    table is ready

--- start.js
+++ start.js
@@ -3,18 +3,16 @@ var fs = require('fs');
 var path = require('path');
 const readline = require('readline');
 
-var Hello = require('./machine');
-hello = new Hello();
- 
 http.createServer(function(req, res){
-
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  res.write('<html><head><meta charset="utf-8"><title>machines</title></head> <body>');
 
   const rl = readline.createInterface({
     input: fs.createReadStream('machines.txt'),
     terminal: true
   });
 
+  res.write('<table border="1">');
   rl.on('line', (line) => {
     var fileName = __dirname.toString() + "/files/" + line + ".txt";
     fs.exists(fileName, function(exists){
@@ -22,13 +20,15 @@ http.createServer(function(req, res){
             //res.write(fileName + ' not exists.');
         }else{
           var data=fs.readFileSync(fileName,"utf-8");
-          res.write(data.toString());
+          //res.write(data.toString());
+          res.write('<tr><td>' + data.toString() + '</td></tr>');
         }
     });
 
   }).on('close', () => {
+    res.write('</table>');
+    res.write('</body></html>');
     res.end();
   });
-
 }).listen(8888);
 
