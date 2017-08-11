0ab87c144a54fd58ebb8220b86ecd831bdfa61cb
ruibz <rui.b.zhang@hotmail.com>
2017-08-11 14:24:42 +0800

    re-commit

--- machine.js
+++ machine.js
@@ -1,10 +1,14 @@
-function Hello() { 
+var util = require('util');
+
+function Hello(req, res) { 
 	var name; 
 	this.setName = function(thyName) { 
 		name = thyName; 
 	}; 
-	this.sayHello = function() { 
-		console.log('Hello ' + name); 
-	}; 
+  this.sayHello = function() { 
+    res.write('hello world');
+    res.end();
+    console.log("hello");
+  }; 
 }; 
 module.exports = Hello;
--- start.js
+++ start.js
@@ -1,38 +1,34 @@
 var http = require('http');
-var fs = require("fs");
+var fs = require('fs');
+var path = require('path');
+const readline = require('readline');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+var Hello = require('./machine');
+hello = new Hello();
+ 
+http.createServer(function(req, res){
 
-    response.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+
+  const rl = readline.createInterface({
+    input: fs.createReadStream('machines.txt'),
+    terminal: true
+  });
+
+  rl.on('line', (line) => {
+    var fileName = __dirname.toString() + "/files/" + line + ".txt";
+    fs.exists(fileName, function(exists){
+        if(!exists){
+            //res.write(fileName + ' not exists.');
+        }else{
+          var data=fs.readFileSync(fileName,"utf-8");
+          res.write(data.toString());
+        }
+    });
+
+  }).on('close', () => {
+    res.end();
+  });
+
 }).listen(8888);
 
-
-console.log("it's done");
-
-/*
-var Client = require('ssh2').Client;
-var conn = new Client();
-conn.on('ready', function() {
-    conn.exec('touch /home/ruibz/node.touch', function(err, stream) {
-        if (err) throw err;
-
-        stream.on('close', function(code, signal) {
-            conn.end();
-        }).on('data', function(data) {
-            console.log('STDOUT: ' + data);
-        }).stderr.on('data', function(data) {
-            console.log('STDERR: ' + data);
-        });
-    });
-}).connect({
-    host: '127.0.0.1',
-    port: 22,
-    username: 'ruibz',
-    password: 'asb#1234'
-    //privateKey: require('fs').readFileSync('/home/admin/.ssh/id_dsa')
-});
-*/
-
-
-console.log('Server running at http://127.0.0.1:8888/');
